7a9b54ede0869e56daef0925dfc8b20496f7e0e0
unknown <iwillia2@WVUP.EDU>
2020-02-24 17:22:24 -0500

    another fix

--- Assignments/Forms/iwilliams/index/my.js
+++ Assignments/Forms/iwilliams/index/my.js
@@ -16,9 +16,9 @@ function test(){
 
 
 function youTyped(){
-    console.log(`Your Input: ${myForm.firstName.value}`);
-    console.log(`Your Input: ${myForm.lastName.value}`);
-    console.log(`Your Input: ${myForm.email.value}`);
+    console.log(`Your First Name: ${myForm.firstName.value}`);
+    console.log(`Your Last Name: ${myForm.lastName.value}`);
+    console.log(`Your Email: ${myForm.email.value}`);
     document.getElementById('preview').innerHTML = `<h2>First Name: <span style="color: green">${myForm.firstName.value}</span></h2>`;
     document.getElementById('preview2').innerHTML = `<h2>Last Name: <span style="color: green">${myForm.lastName.value}</span></h2>`;
     document.getElementById('preview3').innerHTML = `<h2>Email: <span style="color: green">${myForm.email.value}</span></h2>`;

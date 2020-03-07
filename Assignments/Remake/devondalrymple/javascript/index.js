37916f70d3bb0879c50634f0973483f5b4fc4b7e
DevonXDal <devonxpersonal@gmail.com>
2020-03-06 19:35:39 -0500

    HUGE COMMIT:  Work done, four hours at the college

--- Assignments/Remake/devondalrymple/javascript/index.js
+++ Assignments/Remake/devondalrymple/javascript/index.js
@@ -1,4 +1,22 @@
+// Run Javascript Code Only if Ready
 $( document ).ready(function() {
-    $(".dropdown-trigger").dropdown({hover:true, constrainWidth: false}); 
+    // Dropbox Javascript - Set to Open Dropdowns on Hover
+    $(".dropdown-trigger").dropdown({hover:true, constrainWidth: false, coverTrigger: false, inDuration: 300, outDuration: 300});
+    
+    // Slideshow
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+    }
 });
 

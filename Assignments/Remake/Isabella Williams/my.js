de2388bfbed11b38d2f5ff2fb045c663c4dfcf33
unknown <iwillia2@WVUP.EDU>
2020-03-09 14:05:03 -0400

    Fixed The draft

--- Assignments/Remake/Isabella Williams/my.js
+++ Assignments/Remake/Isabella Williams/my.js
@@ -32,3 +32,4 @@ function test(){
         document.getElementById('preview8').innerHTML = `<h2>Thank you. <span style="color: green"></span></h2>`;
 }
 
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>

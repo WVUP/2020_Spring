8805054066d2e3410053c91357a1d78129764f8c
DevonXDal <45339771+DevonXDal@users.noreply.github.com>
2020-03-09 08:41:23 -0400

    Remake progress as of Sunday (#36)
    * Fix Attempt 1
    
    * From button to submit
    
    * Return false to stop reload
    
    * Different code for preventDefault
    
    * Script bugfix attempt
    
    * Added JQuery Library
    
    * Attempt to fix JQuery
    
    * PreventDefault inside onSubmit
    
    * Set it to run after the window loads
    
    * Working Submit Button, added id
    
    * Many failed attempts
    
    * More failures,  attempted fixes
    
    * Completely broken
    
    * Added javascript input display (#29)
    
    * Additional (#30)
    
    * Alignment with folder structure (#31)
    
    * new changes
    
    * new changes
    
    * help
    
    * new
    
    * hey
    
    * Moved to be in parity with windows and class repo
    
    * new changes
    
    * new changes
    
    * help
    
    * new
    
    * hey
    
    * Moved to be in parity with windows and class repo
    
    * forms assignment (#32)
    
    * Done, but perfect
    
    * Done
    
    * Try this again
    
    * ok
    
    * moved assignment to simplify life
    
    * Done, but perfect
    
    * Done
    
    * Try this again
    
    * ok
    
    * moved assignment to simplify life
    
    * Forms Assignment
    
    * removed files
    
    * I could not get the validation to work (#33)
    
    * Massive Change, attempt to resurrect Form assign.
    
    * Fix Attempt 1
    
    * From button to submit
    
    * Return false to stop reload
    
    * Different code for preventDefault
    
    * Script bugfix attempt
    
    * Added JQuery Library
    
    * Attempt to fix JQuery
    
    * PreventDefault inside onSubmit
    
    * Set it to run after the window loads
    
    * Working Submit Button, added id
    
    * Many failed attempts
    
    * More failures,  attempted fixes
    
    * Completely broken
    
    * Added baseline for project
    
    * Work done from no-git computer on Tuesday
    
    * Added a few comments and dropdown test
    
    * Attempt to fix dropdown menu
    
    * Implemented rows and columns and centered content
    
    * Rename and recolor of dropdowns, added searchbar
    
    * Recolored icons
    
    * Attempt to color second nav's background
    
    * Fixed background color of second div
    
    * Added divider
    
    * Got rid of blue text in first div
    
    * Top div text set to black instead of white
    
    * Added space before and after divider
    
    * Renamed ABOUT links
    
    * ABOUT dropdown links added
    
    * Fixed dropdown box list width
    
    * Adding rows for second navbar spacing
    
    * Fixed Home to HOME
    
    * Made the divider in the first nav, less pronounced
    
    * Made divider almost exactly like wvup.edu has it
    
    * HUGE COMMIT:  Work done, four hours at the college
    
    * Added the final image for the slideshow
    
    * Added comments
    
    * Step one in fixing dropdowns
    
    * Prepping for nested dropdowns
    
    * Nested attempt
    
    * Terrible but usable nested dropdown
    
    * Removed nested dropdowns due to severe bugs
    
    * Added names to dropdown items, links for Future
    
    * Added links for current student and new drop item
    
    * Added links for Faculty/Staff
    
    * Alumni links added
    
    * Foundation link added
    
    * Fixed formatting of all the dropdown menus
    
    Co-authored-by: Cameron Plauche <60367693+cplauche@users.noreply.github.com>
    Co-authored-by: iwilliams1423 <59850771+iwilliams1423@users.noreply.github.com>
    Co-authored-by: Portalpower1 <59850800+Portalpower1@users.noreply.github.com>
    Co-authored-by: Benatkinson9 <60367743+Benatkinson9@users.noreply.github.com>
    Co-authored-by: rtate27 <60367745+rtate27@users.noreply.github.com>
    Co-authored-by: unknown <ddalrymp@WVUP.EDU>

---
+++ Assignments/Remake/devondalrymple/javascript/index.js
@@ -0,0 +1,23 @@
+// Devon Dalrymple
+// Run Javascript Code Only if Ready
+$( document ).ready(function() {
+    // Dropbox Javascript - Set to Open Dropdowns on Hover
+    $(".dropdown-trigger").dropdown({hover:true, constrainWidth: false, coverTrigger: false, inDuration: 300, outDuration: 300});
+    
+    // Slideshow
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+    }
+});
+

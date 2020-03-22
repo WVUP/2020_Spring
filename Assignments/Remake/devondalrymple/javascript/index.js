52ed4a06483dc52de870d3234456b28c8b3c8eb5
DevonXDal <devonxpersonal@gmail.com>
2020-03-22 14:55:28 -0400

    Link Tests

--- Assignments/Remake/devondalrymple/javascript/index.js
+++ Assignments/Remake/devondalrymple/javascript/index.js
@@ -26,15 +26,15 @@ $( document ).ready(function() {
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
 function moveToTopFunction() {

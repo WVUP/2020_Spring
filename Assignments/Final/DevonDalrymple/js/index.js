43afa88dbcffc68be6446297456a6ae2617be37c
Devon X. Dalrymple <devonxpersonal@gmail.com>
2020-04-20 12:51:26 -0400

    Half decent side nav

--- Assignments/Final/DevonDalrymple/js/index.js
+++ Assignments/Final/DevonDalrymple/js/index.js
@@ -1,12 +1,25 @@
-$(document).ready(function () {
+var sideToggled;
 
-    
+$(document).ready(function () {
+    sideToggled = false;
 
 });
 // https://www.w3schools.com/howto/howto_js_sidenav.asp
 //Side Navbar Functions
+
+function toggleNav() {
+    if (sideToggled) {
+        closeNav();
+        sideToggled = false;
+    }
+    else {
+        openNav();
+        sideToggled = true;
+    }
+}
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "220px";
 }
   
 function closeNav() {

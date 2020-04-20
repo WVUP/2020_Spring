1b195ead28303e99b38cf2a7896e0e981222c62d
Devon X. Dalrymple <devonxpersonal@gmail.com>
2020-04-20 13:58:44 -0400

    Progress as of 2 pm

--- Assignments/Final/DevonDalrymple/js/index.js
+++ Assignments/Final/DevonDalrymple/js/index.js
@@ -18,10 +18,19 @@ function toggleNav() {
     }
 }
 
+// https://stackoverflow.com/questions/23348456/modify-css-classes-using-javascript
 function openNav() {
     document.getElementById("mySidenav").style.width = "220px";
+    var elements = document.querySelectorAll('.sideText');
+    for(var i=0; i<elements.length; i++){
+    elements[i].style.visibility = "visible";
+}
 }
   
 function closeNav() {
-document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "80px";
+    var elements = document.querySelectorAll('.sideText');
+        for(var i=0; i<elements.length; i++){
+        elements[i].style.visibility = "hidden";
+}
 }

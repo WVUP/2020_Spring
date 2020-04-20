41715862a104e54614db726f6143fc58cf64d790
Devon X. Dalrymple <devonxpersonal@gmail.com>
2020-04-20 10:38:06 -0400

    Sidebar progress made

--- Assignments/Final/DevonDalrymple/js/index.js
+++ Assignments/Final/DevonDalrymple/js/index.js
@@ -3,3 +3,12 @@ $(document).ready(function () {
     
 
 });
+// https://www.w3schools.com/howto/howto_js_sidenav.asp
+//Side Navbar Functions
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}

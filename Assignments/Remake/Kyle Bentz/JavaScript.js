060025e9cd8b5d0b07aaa155e2c467d98cf3301c
KDB000 <kylebentz123@gmail.com>
2020-04-26 14:46:48 -0400

    Remake

---
+++ Assignments/Remake/Kyle Bentz/JavaScript.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    $(".dropdown-trigger").dropdown({hover:true, constrainWidth: false, coverTrigger: false, inDuration: 300, outDuration: 300});
+    
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 5000);
+    }
+  
+  var mybutton = document.getElementById("myButton");
+
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+});

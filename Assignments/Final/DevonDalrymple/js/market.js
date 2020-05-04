081267f97198ce23be0265bde543d6c5b0a6ec8b
Devon X. Dalrymple <devonxpersonal@gmail.com>
2020-05-04 10:13:40 -0400

    Unknown Commit

--- Assignments/Final/DevonDalrymple/js/market.js
+++ Assignments/Final/DevonDalrymple/js/market.js
@@ -15,6 +15,7 @@ $(function(){
     });
 })
 
+// https://www.w3schools.com/howto/howto_js_filter_lists.asp
 function filterFunction() {
     // Declare variables
     var input, filter, div, li, a, i, txtValue;

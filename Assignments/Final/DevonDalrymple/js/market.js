60b5e92809ef309b6b59e34bc3b6858e2342707d
Devon X. Dalrymple <devonxpersonal@gmail.com>
2020-05-04 10:54:57 -0400

    Added Searchable Market Items

--- Assignments/Final/DevonDalrymple/js/market.js
+++ Assignments/Final/DevonDalrymple/js/market.js
@@ -15,7 +15,7 @@ $(function(){
     });
 })
 
-// https://www.w3schools.com/howto/howto_js_filter_lists.asp
+// https://www.w3schools.com/howto/howto_js_filter_lists.asp - Third Times a Charm
 function filterFunction() {
     // Declare variables
     var input, filter, div, li, a, i, txtValue;
